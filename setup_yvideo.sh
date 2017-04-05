d82c9ac2a22369fa2b22ce6f69a3ad237714e8c5
david <david.ostler001@gmail.com>
2017-04-05 12:46:57 -0600

    Adds dev changes; uses ssh keys; Adds comments; Removes SCP from Sartre;

--- setup_yvideo.sh
+++ setup_yvideo.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
+# Create Docker directory (if doesn't exist)
 echo "1. Making Directories..."
 cd ~
 mkdir -p Docker/ && cd Docker
 
+# Clone Dockerfile directorys
 echo
 echo "2. Cloning Dependencies..."
-git clone https://github.com/dro248/ayamelDBDockerfile
-git clone https://github.com/dro248/AyamelDockerfile
-git clone https://github.com/dro248/runAyamel
+git clone git@github.com:dro248/ayamelDBDockerfile
+git clone git@github.com:dro248/AyamelDockerfile
+git clone git@github.com:dro248/runAyamel
 
-echo
-echo "3. Getting secret..."
-scp arclite@sartre4.byu.edu:~/application.conf ./AyamelDockerfile
-
+# Turn off any other mysql database
 echo
 echo "4. Making space for database..."
 sudo service mysql stop
 
+# Run docker-compose file (within runAyamel directory)
 echo
 echo "5. Creating Database & App..."
 cd runAyamel
