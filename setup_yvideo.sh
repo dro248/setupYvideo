349702127b536231922cd660da0e8cef29915b95
David Ostler <david.ostler001@gmail.com>
2017-04-24 14:03:33 -0600

    Changes setup to use Https instead of ssh keys (for github)

--- setup_yvideo.sh
+++ setup_yvideo.sh
@@ -2,18 +2,18 @@
 
 # Create Docker directory (if doesn't exist)
 echo "1. Making Directories..."
-cd ~
+pushd ~
 mkdir -p Docker/ && cd Docker
 
 # Clone Dockerfile directories
 echo
 echo "2. Cloning Dependencies..."
-git clone git@github.com:dro248/ayamelDBDockerfile
+git clone https://github.com/dro248/ayamelDBDockerfile
 
-git clone git@github.com:dro248/AyamelDockerfile
+git clone https://github.com/dro248/AyamelDockerfile
 cd AyamelDockerfile/; git checkout dev; cd ..;
 
-git clone git@github.com:dro248/runAyamel
+git clone https://github.com/dro248/runAyamel
 cd runAyamel; git checkout dev;
 
 
