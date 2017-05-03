b73ec4015bdaa511877622e4f31f2e66fd615984
Santiago Verdu <santiago.verdu.01@gmail.com>
2017-05-03 11:19:40 -0600

    prompt for dependencies

--- setup_yvideo.sh
+++ setup_yvideo.sh
@@ -1,39 +1,53 @@
 #!/bin/bash
 
+dirname(){ 
+    test -n "$1" || return 0
+    local x="$1"; while :; do case "$x" in */) x="${x%?}";; *) break;; esac; done
+    [ -n "$x" ] || { echo /; return; }
+    set -- "$x"; x="${1%/*}"
+    case "$x" in "$1") x=.;; "") x=/;; esac
+    printf '%s\n' "$x"
+}
+
 # Create Docker directory (if doesn't exist)
 echo "1. Making Directories..."
 pushd ~
 mkdir -p Docker/ && cd Docker
 
+git_dir=${GITDIR:-~/Documents/GitHub}
+
 # Clone Dockerfile directories
 echo
 echo "2. Cloning Dependencies..."
-git clone https://github.com/dro248/ayamelDBDockerfile
+git clone https://github.com/dro248/ayamelDBDockerfile &> /dev/null
 
-git clone https://github.com/dro248/AyamelDockerfile
-cd AyamelDockerfile/; git checkout dev; cd ..;
+git clone https://github.com/dro248/AyamelDockerfile &> /dev/null
+cd AyamelDockerfile/; git checkout dev &> /dev/null; cd ..;
 
-git clone https://github.com/dro248/runAyamel
-cd runAyamel; git checkout dev;
+git clone https://github.com/dro248/runAyamel &> /dev/null
+cd runAyamel; git checkout dev &> /dev/null; git checkout -- .
 
+repos=(Ayamel Ayamel.js EditorWidgets subtitle-timeline-editor TimedText)
 
 # Get Ayamel path dir
-read -r -p "Enter path to Ayamel (default: ~/Documents/GitHub/Ayamel):" ayameldir
-if [[ -z $ayameldir ]]; then
-  ayameldir="~/Documents/GitHub/Ayamel"
-fi
+for repo in "${repos[@]}"; do
+    read -r -p "Enter path to $repo (default: ${dir_name:-$git_dir}/$repo): " user_dir
+    if [[ -z "$user_dir" ]]; then
+        user_dir="$git_dir/$repo"
+    else
+        # expand the path
+        if [[ -d "$user_dir" ]]; then
+            user_dir="$( cd "$user_dir"; pwd -P )"
+            dir_name=$(dirname "$user_dir")
+        else
+            user_dir="$git_dir/$repo"
+        fi
+    fi
+    echo "Using $user_dir for $repo."
+    replace "{{$user_dir}}" "$dependenciesdir" -- docker-compose.yml
+done
 
-# Get dependencies path dir
-read -r -p "Enter path to Ayamel (default: /var/www/html):" dependenciesdir
-if [[ -z $dependenciesdir ]]; then
-  dependenciesdir="/var/www/html"
-fi
-
-# Insert $USER into runAyamel/docker-compose.yml file
-replace "{{APP}}" "$ayameldir" -- docker-compose.yml
-replace "{{DEPS}}" "$dependenciesdir" -- docker-compose.yml
-cd ..;
-
+cd ..
 
 # Turn off any other mysql database
 echo
